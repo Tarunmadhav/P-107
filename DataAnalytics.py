c6feb30d16c65951f0e7ebbafd946d20d5e01a7d
Tarun Madhav <65611329+Tarunmadhav@users.noreply.github.com>
2021-03-28 14:05:29 +0530

    Add files via upload

--- DataAnalytics.py
+++ DataAnalytics.py
@@ -1,13 +1,22 @@
 import csv
 import pandas as pd
 import plotly.graph_objects as go
+import plotly_express as px
+
 df=pd.read_csv("data.csv")
 studentdf=df.loc[df["student_id"]=="TRL_xsl"]
-print(mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean())
+print("mean:" , df.groupby(["student_id", "level"], as_index=False)["attempt"].mean())
 figure=go.Figure(go.Bar(
     x=df.groupby(["student_id", "level"], as_index=False)["attempt"].mean(),
-    y=["level"],
+    y=["level1","level2","level3","level4"],
     orientation="h"
 ))
 
 figure.show()
+figure1=px.scatter(
+   df.groupby(["student_id", "level"], as_index=False)["attempt"].mean(),
+   x="student_id",
+   y="level" ,
+   color="attempt"
+)
+figure1.show()
